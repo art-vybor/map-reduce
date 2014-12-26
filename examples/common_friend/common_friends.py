6d485d3cbc685e1022eec525e9dd1cde8cf97f1b
Artyom Vybornov <art-vybor@ya.ru>
2014-12-26 15:14:28 +0300

    add slides

--- examples/common_friend/common_friends.py
+++ examples/common_friend/common_friends.py
@@ -1,15 +1,12 @@
-def map_func(input):
-    [person, friends] = input.split(' - ')
+def map_func(string):
+    [person, friends] = string.split(' - ')
     friends = friends.split()
 
     for friend in friends:
-        result = sorted([person, friend])
-        yield ('%s %s:' % tuple(result), friends)
+        yield ('%s %s: ' % tuple(sorted([person, friend])), friends)
 
 def reduce_func(key, values):
     result = ''
     if len(values) == 2:
         result = ' '.join(set(values[0]).intersection(set(values[1])))
-
-
-    return (key, result)
+    return [(key, result)]
